8276b13d0ef75431cdb4f00831038eef005ce4e5
Alistair McGann <39486445+Alistair-McGann-Tessella@users.noreply.github.com>
2019-12-17 10:42:04 +0000

    Delete dllCopy.bat

--- iocBoot/iocDAQmxBase/dllCopy.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\asyn\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
-XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\support\oncrpc\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
-XCOPY /R /I /Q /Y C:\Instrument\Apps\EPICS\base\master\bin\windows-x64\*.dll %MYDIR%..\..\bin\windows-x64
